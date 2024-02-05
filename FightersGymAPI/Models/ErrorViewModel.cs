b03aabb4938ac084d0e045b49ac10c7881654af0
Fares Ahmed <CDS.FaresAhmed23247@alexu.edu.eg>
2024-02-05 20:41:03 +0200

    Add project files.

---
+++ FightersGymAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FightersGymAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
